2a19e25cc4c6b7ca91e9c4570690676b55034734
Sam DeSota <me@samdesota.com>
2021-05-21 00:13:47 -0400

    exporse remove

--- modules/core/index.ts
+++ modules/core/index.ts
@@ -5,7 +5,7 @@ export { resetAndSeed, buildSchemaAndSeed } from "./reset";
 export type { SeedFn, SeedContext, GlobPath, ResetBasis } from "./reset";
 export type { Model } from "./model";
 export { makeDBTestManager } from "./postgresManager";
-export { insert, update, insertAll, sql } from "./writes";
+export { insert, remove, update, insertAll, sql } from "./writes";
 export type { DBClient } from "./dbClient";
 export { makeClient, closeClient } from "./dbClient";
 export { count } from "./aggregate";
